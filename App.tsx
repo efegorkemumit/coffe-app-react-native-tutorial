23d435f13a6e95419b6727193e574bbebc0d03af
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2025-06-02 02:46:53 +0300

    18- Expo Vector icon kullanımı

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import "./global.css"
+import Entypo from '@expo/vector-icons/Entypo';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text className='bg-red-800'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Entypo name="home" size={24} color="#000" />
     </View>
   );
 }
