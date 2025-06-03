4cd87c034f827da6af84b19b311e03a750240588
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2025-06-03 09:08:21 +0300

    19-reactnavigation kullanımı Bottom tabs stack native stack

--- App.tsx
+++ App.tsx
@@ -2,22 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import "./global.css"
 import Entypo from '@expo/vector-icons/Entypo';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className='bg-red-800'>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Entypo name="home" size={24} color="#000" />
-    </View>
+    <AppNavigator/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
