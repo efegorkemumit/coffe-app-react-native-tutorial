31bb6fcb00e124d1f4ae005583fd6504cf17ec5f
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2025-07-21 23:09:03 +0300

    sentry

--- App.tsx
+++ App.tsx
@@ -3,10 +3,26 @@ import { StyleSheet, Text, View } from 'react-native';
 import "./global.css"
 import Entypo from '@expo/vector-icons/Entypo';
 import AppNavigator from './src/navigation/AppNavigator';
+import * as Sentry from '@sentry/react-native';
 
-export default function App() {
+Sentry.init({
+  dsn: 'https://98449296b27eb4307ddb68474d6943d1@o4509708302876672.ingest.us.sentry.io/4509708324110336',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function App() {
   return (
     <AppNavigator/>
   );
-}
-
+});
